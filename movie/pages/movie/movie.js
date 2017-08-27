8e211dd039e87c9f23310821518ace6b65c2b10b
1125282366 <1125282366@qq.com>
2017-08-27 23:31:08 +0800

    2

--- movie/pages/movie/movie.js
+++ movie/pages/movie/movie.js
@@ -1,4 +1,4 @@
-
+var subjectUtil = require('../../utils/subjectUtil.js');
 Page({
   data: {
     // text:"这是一个页面"
@@ -46,47 +46,9 @@ Page({
       success: function (res) {
         console.log(res);
         var subjec = res.data.subjects;
-        page.processSubjects(subjec);
+        subjectUtil.processSubjects(subjec);
         page.setData({ movies: subjec, hidden: true });
       }
     })
-  },
-  processSubject:function(subject) {
-    var title = subject.title;
-    var directors = subject.directors;
-    var directorStr = "";
-    for (var index in directors) {
-      directorStr = directorStr + directors[index].name + " / ";
-    }
-    if (directorStr != "") {
-      directorStr = directorStr.substring(0, directorStr.length - 2);
-    }
-    var casts = subject.casts;
-    var castStr = "";
-    for (var index in casts) {
-      castStr = castStr + casts[index].name + " / ";
-    }
-    if (castStr != "") {
-      castStr = castStr.substring(0, castStr.length - 2);
-    }
-    var genres = subject.genres;
-    var genresStr = "";
-    for (var index in genres) {
-      genresStr = genresStr + genres[index] + " / ";
-    }
-    if (genresStr != "") {
-      genresStr = genresStr.substring(0, genresStr.length - 2);
-    }
-    var year = subject.year;
-    var text = "名称: " + title + "\n导演: " + directorStr + "\n主演：" + castStr + "\n类型: " + genresStr + "\n 上映年份:" + year + "(中国大陆)";
-    subject.text = text;
-  },
-
-  processSubjects:function(subjec) {
-    for (var i = 0; i < subjects.length; i++) {
-      var subject = subjects[i];
-      this.processSubject(subject);
-    }
   }
-
 })
